393f3390ee92689330cf719abd629b37eaddaa21
bilalayvazoglu <bilal_ayvazoglu@hotmail.com>
2025-01-11 01:35:48 +0300

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",  // Unused vars kuralını devre dışı bırak
+      "prefer-const": "off",  // prefer-const kuralını devre dışı bırak
+    }
+  }
 ];
 
 export default eslintConfig;
